966add418393a19e76c4ee32b69540ccfa34f9fd
Enmanuel Rodriguez <enmaundostres@gmail.com>
2024-12-31 17:01:52 -0400

    wip: copy content of AuthController

--- src/main/java/com/github/enma11235/surveysystemapi/controller/AuthController.java
+++ src/main/java/com/github/enma11235/surveysystemapi/controller/AuthController.java
@@ -1,109 +1,40 @@
 package com.github.enma11235.surveysystemapi.controller;
 
-import com.github.enma11235.surveysystemapi.dto.request.LoginRequestDTO;
-import com.github.enma11235.surveysystemapi.model.User;
+import com.github.enma11235.surveysystemapi.dto.request.LoginRequestBody;
+import com.github.enma11235.surveysystemapi.service.AuthService;
 import com.github.enma11235.surveysystemapi.service.UserService;
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
+
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import javax.crypto.SecretKey;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("api/auth")
 public class AuthController {
     private final UserService userService;
-    public final SecretKey secretKey;
+    private final AuthService authenticationService;
+
 
     @Autowired
-    public AuthController(UserService userService, SecretKey secretKey) {
+    public AuthController(UserService userService, AuthService authenticationService) {
         this.userService = userService;
-        this.secretKey = secretKey;
+        this.authenticationService = authenticationService;
     }
 
-    // Endpoint to log a user into the system
+    // LOG IN
     @PostMapping
-    public ResponseEntity<Void> login(@RequestBody LoginRequestDTO body) {
-        Optional<User> user = userService.findUserByNickname(body.getNickname());
+    public ResponseEntity<Void> login(@RequestBody @Valid LoginRequestBody body) {
+        String token = authenticationService.authenticate(body.getNickname(), body.getPassword());
+        String cookie = "token=" + token + "; HttpOnly; Secure; Path=/; SameSite=Strict";
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(HttpHeaders.SET_COOKIE, cookie);
 
-        if (user.isPresent()) {
-            if (user.get().getPassword().equals(body.getPassword())) {
-                // Fecha actual
-                Date now = new Date();
-
-                // Calcular fecha de expiración (1 día después)
-                Calendar calendar = Calendar.getInstance();
-                calendar.setTime(now);
-                calendar.add(Calendar.DAY_OF_YEAR, 1); // Añadir 1 día
-                Date expirationDate = calendar.getTime();
-                String token = Jwts.builder()
-                        .setSubject(user.get().getNickname())
-                        .setIssuedAt(new Date())
-                        .setExpiration(expirationDate)
-                        .signWith(SignatureAlgorithm.HS512, secretKey)
-                        .compact();
-                HttpHeaders headers = new HttpHeaders();
-                headers.add("Set-Cookie", "jwt=" + token + "; HttpOnly; Path=/; Max-Age=604800");
-                return ResponseEntity.status(HttpStatus.OK).headers(headers).build();
-            } else {
-                return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
-            }
-        } else {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
-        }
+        // Devolver la respuesta con los headers
+        return ResponseEntity.status(HttpStatus.OK).headers(headers).build();
     }
 
-    public boolean isAuthenticated(User user, String token) {
-            try {
-                Claims claims = Jwts.parserBuilder()
-                        .setSigningKey(secretKey)
-                        .build()
-                        .parseClaimsJws(token)
-                        .getBody();
-                String nickname = claims.getSubject();
-                if (user.getNickname().equals(nickname)) {
-                    return true;
-                } else {
-                    return false;
-                }
-            } catch (Exception e) {
-                return false;
-            }
-
-    }
-
-    public String authenticate(User user) {
-        // Fecha actual
-        Date now = new Date();
-
-        // Calcular fecha de expiración (1 día después)
-        Calendar calendar = Calendar.getInstance();
-        calendar.setTime(now);
-        calendar.add(Calendar.DAY_OF_YEAR, 1); // Añadir 1 día
-        Date expirationDate = calendar.getTime();
-
-        return Jwts.builder()
-                .setSubject(user.getNickname())
-                .setIssuedAt(new Date())
-                .setExpiration(expirationDate)
-                .signWith(SignatureAlgorithm.HS512, secretKey)
-                .compact();
-    }
-
-    public String getNicknameFromToken(String token) {
-        Claims claims = Jwts.parserBuilder()
-                .setSigningKey(secretKey)
-                .build()
-                .parseClaimsJws(token)
-                .getBody();
-        return claims.getSubject();
-    }
 }
